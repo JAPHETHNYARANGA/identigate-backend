118ac2117465c1346b2e1de4221b340b23529742
japhethnyaranga <nyaranga4@gmail.com>
2023-04-27 12:04:20 +0300

    created login register and logout routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Authentication
+Route::post('login', [Authentication::class,'login']);
+
+Route::post('register',[Authentication::class, 'register']);
+
+Route::get('logout', [Authentication::class,'logout'])->middleware('auth:api');
+
+
+
