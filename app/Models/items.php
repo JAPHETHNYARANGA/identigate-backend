fcb200d650671dcdade25dee25c1ec188bffeed3
japhethnyaranga <nyaranga4@gmail.com>
2023-04-27 12:02:08 +0300

    filled the items model with name and dscription image will be added later

--- app/Models/items.php
+++ app/Models/items.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class items extends Model
 {
     use HasFactory;
+
+    protected $table = 'items';
+
+    protected $fillable = [
+        'id', 'userId', 'name', 'description'
+    ];
 }
