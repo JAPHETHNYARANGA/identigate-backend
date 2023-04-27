06d914ca70c5369548a8f736ad6de858718895f7
japhethnyaranga <nyaranga4@gmail.com>
2023-04-27 12:11:16 +0300

    created login function, will install passport tokens later

--- app/Http/Controllers/authentication.php
+++ app/Http/Controllers/authentication.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class authentication extends Controller
 {
     //
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' =>'required'
+        ]);
+
+        $email = $request['email'];
+        $user = User::where('email', $email)->firstOrFail();
+        $token = $user->createToken('API TOKEN')->accessToken;
+
+        $credentials = $request->only('email', 'password');
+
+        if(Auth::attempt($credentials)){
+            return response(
+                [
+                    'success'=>true,
+                    'message'=>'User logged in successfully',
+                    'user' =>$user,
+                    'token' => $token
+                ], 200
+            );
+        }else{
+            return response(
+                [
+                    'success' =>false,
+                    'message'=>'Login Failed'
+                ]
+            )
+        }
+    }
 }
